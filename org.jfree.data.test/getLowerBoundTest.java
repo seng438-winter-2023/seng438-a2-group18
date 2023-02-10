8c22383dadb68cebbb31cab0ef7deef76a3686ce
Room <108599236+codeongaku25@users.noreply.github.com>
2023-02-10 16:58:27 -0700

    Update getLowerBoundTest.java

--- org.jfree.data.test/getLowerBoundTest.java
+++ org.jfree.data.test/getLowerBoundTest.java
@@ -10,9 +10,7 @@ public class getLowerBoundTest {
 	private Range rangeZ;
 	private Range rangeP;
 	private Range rangeDoub;
-	private Range rangeDiff;
-	private Range rangeDiff2;
-
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -23,8 +21,6 @@ public class getLowerBoundTest {
 		rangeZ = new Range(0, 0);      // range for lower and upper bound being 0
 		rangeP = new Range(1, 10);     // range for positive lower bound
 		rangeDoub = new Range(2.5, 5.0);     // range for double values
-		rangeDiff = new Range(4.2, 6);   // range for a double and an integer 
-		rangeDiff2 = new Range(1, 2.8);   // range for a double and an integer 
 	}
 	
 	@Test
@@ -45,16 +41,6 @@ public class getLowerBoundTest {
         @Test
         public void testDoubleForLowerBound() { // Lower bound being a double
 		assertEquals("Expecting it to return 2.5", 2.5, rangeDoub.getLowerBound(), .000000001d);
-        }
-    
-        @Test
-        public void testMixedForLowerBound() { // Lower bound being a double, Upper bound being an integer
-		assertEquals("Expecting it to return 4.2", 4.2, rangeDiff.getLowerBound(), .000000001d);
-        }
-    
-        @Test
-        public void testMixed2ForLowerBound() { // Lower bound being an integer, Upper bound being a double
-		assertEquals("Expecting it to return 1", 1, rangeDiff2.getLowerBound(), .000000001d);
         }
     
         @After
